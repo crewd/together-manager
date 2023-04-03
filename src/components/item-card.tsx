131c1128aad220b19b5c300c9a86b59c6899dec6
crewd <newspaper321@nate.com>
2023-04-03 23:58:54 +0900

    style: prettier-tailwindcss

--- src/components/item-card.tsx
+++ src/components/item-card.tsx
@@ -4,17 +4,17 @@ import { Store } from '../types/store.type';
 
 function ItemCard({ data }: { data: Store }) {
   return (
-    <div className="w-[320px] h-[180px] border p-[16px] rounded-xl shadow-sm cursor-pointer hover:shadow-md flex flex-col justify-center">
+    <div className="flex h-[180px] w-[320px] cursor-pointer flex-col justify-center rounded-xl border p-[16px] shadow-sm hover:shadow-md">
       <div className="flex justify-between">
-        <p className="text-2xl font-bold py-[10px]">{data.name}</p>
-        <div className="flex justify-between w-[70px]">
-          <button className="hover:text-blue-500 hover:font-bold">수정</button>
-          <button className="hover:text-red-500 hover:font-bold">삭제</button>
+        <p className="py-[10px] text-2xl font-bold">{data.name}</p>
+        <div className="flex w-[70px] justify-between">
+          <button className="hover:font-bold hover:text-blue-500">수정</button>
+          <button className="hover:font-bold hover:text-red-500">삭제</button>
         </div>
       </div>
       <p className="py-[10px]">{data.address}</p>
       <div className="flex py-[10px]">
-        <FontAwesomeIcon className="w-[25px] h-[25px]" icon={faUsers} />
+        <FontAwesomeIcon className="h-[25px] w-[25px]" icon={faUsers} />
         <p className="pl-[6px]">
           직원 수:
           <span className="pl-[10px] pr-[6px]">{data.members}</span>명
