734a95255d2e945f3bb3635f026fdcfdf4afe6dc
crewd <newspaper321@nate.com>
2023-04-21 04:36:50 +0900

    feat: editor custom toolbar

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,12 +1,23 @@
+import { useMemo } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 function Editor() {
-  return (
-    <div>
-      <ReactQuill theme="snow" />
-    </div>
-  );
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: [
+          [{ header: [1, 2, 3, false] }],
+          ['bold', 'italic', 'underline', 'strike'],
+          ['blockquote'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          [{ color: [] }, { background: [] }],
+          [{ align: [] }, 'link', 'image'],
+        ],
+      },
+    };
+  }, []);
+  return <ReactQuill theme="snow" modules={modules} />;
 }
 
 export default Editor;
