5d951f0387cf054ed7fef44a08f6dcf8a4f37135
crewd <newspaper321@nate.com>
2023-04-21 04:22:48 +0900

    design: editor css 수정

--- src/pages/create-notice-page.tsx
+++ src/pages/create-notice-page.tsx
@@ -5,14 +5,14 @@ function CreateNoticePage() {
   return (
     <div className="container mx-auto max-w-[1024px]">
       <h2 className="text-2xl font-bold">공지사항</h2>
-      <div className="mt-6 shadow-md">
+      <div className="mt-6 bg-white shadow-md">
         <input
           type="text"
           placeholder="제목"
           className="w-full border border-b-0 border-[#ccc] px-3 py-3 outline-none"
         />
-        <div className="h-[542.84px] bg-white">
-          <ReactQuill style={{ height: '500px' }} theme="snow" />
+        <div className="w-full">
+          <ReactQuill theme="snow" />
         </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
