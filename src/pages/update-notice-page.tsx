9307a04fe6bf59f71d7202ba677464a5cdf6ac53
crewd <newspaper321@nate.com>
2023-05-03 01:19:47 +0900

    feat: 공지사항 수정

--- src/pages/update-notice-page.tsx
+++ src/pages/update-notice-page.tsx
@@ -1,21 +1,51 @@
 import ReactQuill from 'react-quill';
 import Editor from '../components/editor';
-import { useRef } from 'react';
-import { Notice } from '../types/notice.type';
+import { useRef, useState } from 'react';
+import { Notice, NoticeFormData } from '../types/notice.type';
 import { useSelector } from 'react-redux';
 import { RootState, useAppDispatch } from '../store/store';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { updateNotice } from '../store/modules/notice-reducer';
 
 function UpdateNoticePage() {
-  const editorRef = useRef<ReactQuill>(null);
-
-  const { noticeId } = useParams();
+  const { storeId, noticeId } = useParams();
 
   const notice: Notice = useSelector(
     (state: RootState) => state.noticeReducer.notices,
   ).filter((notice) => notice.noticeId === noticeId)[0];
 
+  const [title, setTitle] = useState<string>(notice.title);
+  const editorRef = useRef<ReactQuill>(null);
+
+  const navigate = useNavigate();
+
   const dispatch = useAppDispatch();
+
+  const onChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const updateNoticeSubmit = async () => {
+    if (!noticeId) {
+      return navigate('/');
+    }
+    if (!title) {
+      return alert('제목을 입력해 주세요');
+    }
+
+    if (
+      !editorRef.current?.getEditor().getText() ||
+      editorRef.current?.getEditor().root.innerHTML === '<p><br></p>'
+    ) {
+      return alert('내용을 입력해 주세요');
+    }
+    const noticeData: NoticeFormData = {
+      title: title,
+      content: editorRef.current?.getEditor().root.innerHTML,
+    };
+    dispatch(updateNotice({ noticeData, noticeId }));
+    navigate(`/store/${storeId}/notice`);
+  };
 
   return (
     <div className="container mx-auto max-w-[1024px]">
@@ -25,12 +55,22 @@ function UpdateNoticePage() {
           type="text"
           placeholder="제목"
           className="w-full border border-b-0 border-[#ccc] px-3 py-3 outline-none"
-          defaultValue={notice.title}
+          defaultValue={title}
+          onChange={onChangeTitle}
         />
         <Editor editorRef={editorRef} defaultValue={notice.content} />
       </div>
       <div className="mt-6 flex justify-end gap-4">
-        <button className="rounded-md border bg-blue-500 px-8 py-2 text-white shadow transition-colors duration-200">
+        <button
+          className="rounded-md border bg-white px-6 py-2 shadow transition-colors duration-200 hover:bg-red-500 hover:text-white"
+          onClick={() => navigate(`/store/${storeId}/notice/${noticeId}`)}
+        >
+          취소
+        </button>
+        <button
+          className="rounded-md border bg-blue-500 px-6 py-2 text-white shadow transition-colors duration-200"
+          onClick={updateNoticeSubmit}
+        >
           수정
         </button>
       </div>
