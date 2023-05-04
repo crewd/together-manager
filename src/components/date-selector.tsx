3d9242d066f6b846dcce8a7d6aea76c3e2e13fa7
crewd <newspaper321@nate.com>
2023-05-04 20:00:00 +0900

    feat: datepicker component

---
+++ src/components/date-selector.tsx
@@ -0,0 +1,24 @@
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { ko } from 'date-fns/esm/locale';
+import { useState } from 'react';
+
+function DateSelector() {
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+
+  return (
+    <div className="w-[180px]">
+      <DatePicker
+        className="w-[180px] text-xl font-bold focus:bg-white focus:outline-none"
+        locale={ko}
+        dateFormat="yyyy년 MM월 dd일"
+        selected={startDate}
+        startDate={startDate}
+        onChange={(date) => setStartDate(date)}
+        popperPlacement="bottom"
+      />
+    </div>
+  );
+}
+
+export default DateSelector;
--- src/pages/take-over-page.tsx
+++ src/pages/take-over-page.tsx
@@ -1,8 +1,13 @@
+import DateSelector from '../components/date-selector';
+
 function TakeOverPage() {
   return (
     <div className="container mx-auto max-w-[1024px]">
       <div className="flex items-center justify-between pb-6">
         <h2 className="text-2xl font-bold">인수인계</h2>
+      </div>
+      <div className="w-full rounded-md border bg-white p-4 shadow">
+        <DateSelector />
       </div>
     </div>
   );
