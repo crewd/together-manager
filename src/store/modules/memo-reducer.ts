4dd8e9efbb3429584b01b6723cbb0b39febea3c5
crewd <newspaper321@nate.com>
2023-05-09 01:31:33 +0900

    feat: memo reducer 추가

--- src/store/modules/memo-reducer.ts
+++ src/store/modules/memo-reducer.ts
@@ -1,9 +1,10 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import moment from 'moment';
 import { v4 } from 'uuid';
 
 const initialState: {
   memos: {
+    memoId: string;
     content: string;
     date: string;
     author: string;
@@ -20,17 +21,45 @@ const initialState: {
 
 export const addMemo = createAsyncThunk(
   'memo/add',
-  async ({ content }: { content: string }) => {
+  async ({ content, date }: { content: string; date: Date }) => {
     if (!content) {
       throw new Error('not found content');
     }
+    if (date.toDateString() !== new Date().toDateString()) {
+      throw new Error('mismatch date');
+    }
     const newMemo = {
       content: content,
-      contentId: v4(),
+      memoId: v4(),
       date: moment().format('YYYY-MM-DD HH:mm'),
       author: '작성자명',
+      checked: false,
+      completer: '',
     };
 
     return newMemo;
   },
 );
+
+const memoSlice = createSlice({
+  name: 'memo',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // 인수인계 추가
+      .addCase(addMemo.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(addMemo.fulfilled, (state, action) => {
+        state.memos = [...state.memos, action.payload];
+        state.isLoading = false;
+      })
+      .addCase(addMemo.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default memoSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -3,12 +3,14 @@ import { useDispatch } from 'react-redux';
 import authReducer from './modules/auth-reducer';
 import storeReducer from './modules/store-reducer';
 import noticeReducer from './modules/notice-reducer';
+import memoReducer from './modules/memo-reducer';
 
 const store = configureStore({
   reducer: {
     authReducer: authReducer,
     storeReducer: storeReducer,
     noticeReducer: noticeReducer,
+    memoReducer: memoReducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
