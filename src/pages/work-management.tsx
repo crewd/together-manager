6eec62ec4df5dff9c079dae5598c3bba03f94a8a
crewd <newspaper321@nate.com>
2023-05-15 20:32:07 +0900

    design: 직종추가 버튼

--- src/pages/work-management.tsx
+++ src/pages/work-management.tsx
@@ -1,8 +1,19 @@
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 function WorkManagement() {
   return (
     <div className="container mx-auto max-w-[1024px]">
-      <h2 className="pb-6 text-2xl font-bold">💼 업무관리</h2>
-      <div className="">직종 추가</div>
+      <div className="flex items-center justify-between pb-4">
+        <h2 className="text-2xl font-bold">💼 업무관리</h2>
+        <button className="px-3 py-2 bg-white border rounded-md shadow hover:bg-blue-500 hover:text-white">
+          <FontAwesomeIcon className="mr-2" icon={faPlus} />
+          직종추가
+        </button>
+      </div>
+      <div className="w-full p-4 bg-white border rounded-md shadow">
+        <p className="text-lg font-bold text-gray-500">업무를 추가해 주세요</p>
+      </div>
     </div>
   );
 }
